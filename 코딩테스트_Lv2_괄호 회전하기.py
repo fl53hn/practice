7db65248f9fadbbe6742f3f54f00f7b61dcd4e3e
fl53hn <116294973+fl53hn@users.noreply.github.com>
2024-01-26 13:28:54 +0900

    Create 코딩테스트_Lv2_괄호 회전하기.py

---
+++ 코딩테스트_Lv2_괄호 회전하기.py
@@ -0,0 +1,8 @@
+def solution(s):
+    answer = -1
+    for i in range(len(s)):
+        if s[0] == ']' or s[0] == ')' or s[0] == '}':
+            continue
+        else:
+            
+    return answer
