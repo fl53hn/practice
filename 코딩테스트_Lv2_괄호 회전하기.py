a2517e7b279551d6a96bbe8de2e81073c5f4c916
fl53hn <116294973+fl53hn@users.noreply.github.com>
2024-01-28 17:48:12 +0900

    Update 코딩테스트_Lv2_괄호 회전하기.py

--- 코딩테스트_Lv2_괄호 회전하기.py
+++ 코딩테스트_Lv2_괄호 회전하기.py
@@ -1,8 +1,37 @@
-def solution(s):
-    answer = -1
-    for i in range(len(s)):
-        if s[0] == ']' or s[0] == ')' or s[0] == '}':
-            continue
-        else:
+# def solution(s):
+#     answer = -1
+#     for i in range(len(s)):
+#         if s[0] == ']' or s[0] == ')' or s[0] == '}':
+#             continue
+#         else:
             
-    return answer
+#     return answer
+
+def check(str_list):
+    stack = []
+
+    for _str in str_list:
+        if _str in ('[', '(', '{'):
+            stack.append(_str)
+        else:
+            if not stack: return False
+            x = stack.pop()
+            if _str == ']' and x != '[':
+                return False
+            elif _str == ')' and x != '(':
+                return False
+            elif _str == '}' and x != '{':
+                return False
+
+    if stack: return False
+    return True
+
+def solution(s):
+    cnt = 0
+    for i in range(len(s)):
+        u = s[:i]
+        v = s[i:]
+
+        if check(v+u):
+            cnt +=1
+    return cnt
