80d8bb2c26270d2efbe1a295edcf95138b6ad383
fl53hn <116294973+fl53hn@users.noreply.github.com>
2024-02-06 16:54:00 +0900

    Update 코딩테스트_Lv2_피로도.py

--- 코딩테스트_Lv2_피로도.py
+++ 코딩테스트_Lv2_피로도.py
@@ -14,3 +14,31 @@ def dfs(dungeons, i, k, Visited):
             k -= dungeons[i][1]
             if not Visited[i]:
                 dfs(graph, k, Visited)
+
+######
+def solution(k, dungeons):
+    Visited = [0 for i in range(len(dungeons))]
+    an = []
+    for i in range(len(dungeons)):
+        Visited = [0 for i in range(len(dungeons))]
+        an.append(dfs(i, k, dungeons, Visited))
+        print("hh")
+
+        
+def dfs(i, k, dungeons, Visited):
+    answer = 0
+    print(Visited)
+    for j in range(len(dungeons)):
+        if not Visited[i] == 1:
+            if k >= dungeons[i][0]:
+                Visited[i] = 1
+                k = k - dungeons[i][1]
+                answer += 1
+                dfs(j, k, dungeons, Visited)
+            else:
+                continue
+        else:
+            continue
+    return answer
+        
+        
