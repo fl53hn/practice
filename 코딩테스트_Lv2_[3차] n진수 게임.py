073b01aa95b7d4f7ce257d96ce1533902aa71973
fl53hn <116294973+fl53hn@users.noreply.github.com>
2024-02-11 15:42:42 +0900

    Update 코딩테스트_Lv2_[3차] n진수 게임.py

--- 코딩테스트_Lv2_[3차] n진수 게임.py
+++ 코딩테스트_Lv2_[3차] n진수 게임.py
@@ -1,18 +1,43 @@
-import string
-tmp = string.digits+string.ascii_lowercase
-
 def solution(n, t, m, p):
-    answer = []
-    for i in range(1, t+1):
-        result = (i-1)*m + (p-1)
-        answer.append(convert(result, n))
-        
+    answer = ''
+    game = ''
+    cur = p - 1
+    for num in range(t * m):
+        game += convert(num, n)
+    while 1:
+        if len(answer) == t:
+            break
+        answer += game[cur]
+        cur += m
     return answer
 
 
-def convert(num, base) :
-    q, r = divmod(num, base)
-    if q == 0 :
-        return tmp[r] 
-    else :
-        return convert(q, base) + tmp[r]
+def convert(number, n):
+    if number == 0:
+        return '0'
+    NUMBERS = "0123456789ABCDEF"
+    res = ""
+    while number > 0:
+        number, mod = divmod(number, n)
+        res += NUMBERS[mod]
+    return res[::-1]
+
+
+# import string
+# tmp = string.digits+string.ascii_lowercase
+
+# def solution(n, t, m, p):
+#     answer = []
+#     for i in range(1, t+1):
+#         result = (i-1)*m + (p-1)
+#         answer.append(convert(result, n))
+        
+#     return answer
+
+
+# def convert(num, base) :
+#     q, r = divmod(num, base)
+#     if q == 0 :
+#         return tmp[r] 
+#     else :
+#         return convert(q, base) + tmp[r]
