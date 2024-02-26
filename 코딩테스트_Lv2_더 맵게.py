693959ea237f8169a7e13dd774c34216e2282a1e
fl53hn <116294973+fl53hn@users.noreply.github.com>
2024-02-26 16:18:15 +0900

    Update 코딩테스트_Lv2_더 맵게.py

--- 코딩테스트_Lv2_더 맵게.py
+++ 코딩테스트_Lv2_더 맵게.py
@@ -1,5 +1,34 @@
+import heapq
 def solution(scoville, K):
     answer = 0
+    heapq.heapify(scoville)
+    
+    while scoville[0]<K:
+        mix = heapq.heappop(scoville) + (heapq.heappop(scoville) * 2)
+        heapq.heappush(scoville, mix)
+        answer+=1
+        if len(scoville) == 1 and scoville[0] < K:
+            return -1
+        
     return answer
 
-#소마 작성...
+# from collections import deque
+# def solution(scoville, K):
+#     answer = 0
+#     sco = 0
+#     scoville.sort()
+#     for i in range(0, len(scoville)):
+#         if K<sco and K< scoville[i]:
+#             return answer
+#         elif sco ==0:
+#             sco = (scoville[i] + scoville[i+1]*2)
+#             answer+=1
+#         else:
+#             if sco < scoville[i+1]:
+#                 sco = sco + scoville[i+1]*2
+#                 answer +=1
+#             else:
+#                 sco = scoville[i+1] + sco*2
+#                 answer +=1
+#         if i == (len(scoville)-1):
+#             return -1
