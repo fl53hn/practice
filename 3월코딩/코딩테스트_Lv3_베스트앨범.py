b18f3038682ce76af3a0c8a4895e73f6c03b82f5
fl53hn <116294973+fl53hn@users.noreply.github.com>
2024-03-30 18:24:28 +0900

    Create 코딩테스트_Lv3_베스트앨범.py

---
+++ 3월코딩/코딩테스트_Lv3_베스트앨범.py
@@ -0,0 +1,35 @@
+def solution(genres, plays):
+    answer = []
+    dic1 = {}
+    dic2 = {}
+    for i, (g, p) in enumerate(zip(genres, plays)):
+        if g not in dic1:
+            dic1[g] = [(i, p)]
+        else:
+            dic1[g].append((i, p))
+
+        if g not in dic2:
+            dic2[g] = p
+        else:
+            dic2[g] += p
+            
+    for (k, v) in sorted(dic2.items(), key=lambda x:x[1], reverse=True):
+        for (i, p) in sorted(dic1[k], key=lambda x:x[1], reverse=True)[:2]:
+            answer.append(i)
+
+    return answer
+
+    
+    return answer
+
+# def solution(genres, plays):
+#     answer = []
+#     dic = {}
+    
+#     for i in range(len(genres)):
+#         if genres[i] not in dic:
+#             dic[genres[i]] = [plays[i],i]
+#         else:
+#             dic[genres[i]][0] += plays[i]
+#     print(dic)
+#     return answer
