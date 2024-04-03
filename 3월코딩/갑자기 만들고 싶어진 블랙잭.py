cc65f45eb4b0914c1a84d8c0e05c7cbe818c182c
JaeSung Lee <fl53hn@naver.com>
2024-04-03 20:15:29 +0900

    Update 갑자기 만들고 싶어진 블랙잭.py

--- 3월코딩/갑자기 만들고 싶어진 블랙잭.py
+++ 3월코딩/갑자기 만들고 싶어진 블랙잭.py
@@ -1,3 +1,17 @@
-import tkinter as tkt
+## 베팅금액 정하기
+## 배당율은 건 금액만큼을 받는다
+## K,Q,J는 10, A는 1 or 11
+## 시작 시 카드 2장 지급
+## 21 초과시 버스트, 유저 패배
+## 둘 다 21일 시 무승부
+## 힛, 스테이, 더블 다운, 버스트 3가지 구현
+from tkinter import *
 import random
 
+tk = Tk()
+# 2. 텍스트 표시
+label = Label(tk,text='Hello World!')
+# 3. 레이블 배치 실행
+label.pack()
+# 4. 메인루프 실행
+tk.mainloop()
