ab48390f7168947c8c5e5a25a9fdf1303cdf36bd
fl53hn <116294973+fl53hn@users.noreply.github.com>
2024-04-07 01:35:19 +0900

    Update 코딩테스트_Lv3_기지국 설치.py

--- 3월코딩/코딩테스트_Lv3_기지국 설치.py
+++ 3월코딩/코딩테스트_Lv3_기지국 설치.py
@@ -1,25 +1,42 @@
-import math
-# 1. 그리디?
-# N: 200,000,000
-# 1 ~ (2*w)+1 설치, 6~10 사이 있음, 
-## stations안에 1 이나 n일 경우가 동작하지 않음, 수정예정
+# 밑에 방식 말고 dic에 -w ~ +w 형식으로 저장 후, 남은 배열 추출. 추출된 배열 % 2w+1 해서 구하기
 def solution(n, stations, w):
-    i,j = 1, 0
-    answer = 0
-    while i <= n:
-        if (i + (2*w)) >= max(1, stations[j] - w) and min(n, stations[j]+w) >= i:
-            if stations[j] == (i + w):
-                print(i)
-                i = i + 2*w + 1
-            else:
-                answer+=1
-                i = stations[j] + w + 1
-            if j < len(stations)-1:
-                j+=1
-            elif j == len(stations)-1:
-                answer += math.ceil((n - (stations[j]+w))/(2*w+1))
-                break
-        else:
-            answer +=1
-            i += 2*w + 1
+    answer,i = 0,1
+    array = []
+    dic = {}
+    for station in stations:
+        dic[int(station-w)] = int(station+w)
+    # for i in range(len(dic)):
+    #     # dic[i] == 5
+    #     # dic[]
+    for d in dic:
+        array.append(d - i)
+        i += dic[d]
+        
+    print(array)
     return answer
+
+# import math
+# # 1. 그리디?
+# # N: 200,000,000
+# # 1 ~ (2*w)+1 설치, 6~10 사이 있음, 
+# def solution(n, stations, w):
+#     i,j = 1, 0
+#     answer = 0
+#     while i <= n:
+#         if (i + (2*w)) >= (stations[j] - w) and (stations[j]+w) >= i:
+#             if stations[j] == (i + w):
+#                 print(i)
+#                 i = i + 2*w + 1
+#             else:
+#                 if stations[j] > 2*w:
+#                     answer+=1
+#                 i = stations[j] + w + 1
+#             if j < len(stations)-1:
+#                 j+=1
+#             elif j == len(stations)-1:
+#                 answer += math.ceil((n - (stations[j]+w))/(2*w+1))
+#                 break
+#         else:
+#             answer +=1
+#             i += 2*w + 1
+#     return answer
