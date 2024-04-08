32081428bf6a1ab46d14f39866a05b0f37daf106
fl53hn <116294973+fl53hn@users.noreply.github.com>
2024-04-08 17:44:32 +0900

    Update 코딩테스트_Lv3_기지국 설치.py

--- 3월코딩/코딩테스트_Lv3_기지국 설치.py
+++ 3월코딩/코딩테스트_Lv3_기지국 설치.py
@@ -1,19 +1,44 @@
-# 밑에 방식 말고 dic에 -w ~ +w 형식으로 저장 후, 남은 배열 추출. 추출된 배열 % 2w+1 해서 구하기
+import math
 def solution(n, stations, w):
-    answer,i = 0,1
-    array = []
-    dic = {}
-    for station in stations:
-        dic[int(station-w)] = int(station+w)
-    # for i in range(len(dic)):
-    #     # dic[i] == 5
-    #     # dic[]
-    for d in dic:
-        array.append(d - i)
-        i += dic[d]
+    answer = 0
+    V = [0 for station in range(len(stations)+1)]
+    title = 1
+    
+    for i in range(0, len(stations)):
+        if stations[i] - w > 1:
+            V[i] = stations[i] - w - title
+            title = stations[i] + w + 1
+        else:
+            title = stations[i] + w + 1
+    if n-title+1 > 0:
+        V[-1] = n - title + 1
         
-    print(array)
+    for i in range(len(V)):
+        answer+=math.ceil(V[i]/(2*w+1))
+    
     return answer
+
+
+
+# # 밑에 방식 말고 dic에 -w ~ +w 형식으로 저장 후, 남은 배열 추출. 추출된 배열 % 2w+1 해서 구하기
+# def solution(n, stations, w):
+#     answer,i = 0,1
+#     array = []
+#     dic = {}
+#     for station in stations:
+#         dic[int(station-w)] = int(station+w)
+#     # for i in range(len(dic)):
+#     #     # dic[i] == 5
+#     #     # dic[]
+#     print(dic)
+#     for d in dic:
+#         print(d-i)
+#         array.append(d - i)
+#         i += dic[d]
+#         print(i,"2")
+
+#     print(array)
+#     return answer
 
 # import math
 # # 1. 그리디?
