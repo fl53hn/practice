25c2fb60151544b1d89fb2b9b9074115f5f7f973
fl53hn <116294973+fl53hn@users.noreply.github.com>
2024-05-03 21:39:06 +0900

    Update 코딩테스트_Lv4_매출 하락 최소화.py

--- 3월코딩/코딩테스트_Lv4_매출 하락 최소화.py
+++ 3월코딩/코딩테스트_Lv4_매출 하락 최소화.py
@@ -1,1 +1,19 @@
 
+def solution(sales, links):
+    answer = 0
+    return answer
+
+#def dfs(v):
+    #변수 선언
+
+    #for문으로 자식 정점 순회
+#        dfs(k) #자식 정점을 dfs를 보냄, 그래야 자식 정점의 매출액 경우의 수를 가져올 수 있음.
+
+#        합산변수 = min(DP[k][1], DP[k][0]) #k를 순회하며 자식 정점의 매출액 중 가장 적은 값을 더함.
+
+#    경우1) 수행
+
+    #if 조건문:
+       #경우2) 수행
+    #else:
+       #경우3 수행
